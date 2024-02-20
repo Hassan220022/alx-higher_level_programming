aff8f894c3e45862539c157afad21b1b232207f2
Hassan220022 <hassansonson2002@gmail.com>
2024-02-21 00:01:54 +0200

    added a new line

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
+for letter in range(ord('a'), ord('z')+1):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+        print("{:s}".format(chr(letter)), end="")
+    else:
+        continue
