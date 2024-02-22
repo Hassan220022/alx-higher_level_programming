20104d40d035719ce6d4e0d40ded6e2d9bc574fb
Hassan220022 <hassansonson2002@gmail.com>
2024-02-22 22:01:44 +0200

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    counter = len(sys.argv) - 1
+
+    if counter == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(counter))
+        for i in range(counter):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
