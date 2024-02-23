6af985f8a677ca7a9aea46727a67a5e62d066054
Hassan220022 <hassansonson2002@gmail.com>
2024-02-23 14:39:28 +0200

    bor3i

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import sys
-
-# Function to add all arguments
-def infinite_add():
-    # Start with a total sum of 0
-    total_sum = 0
-    # Iterate over all arguments starting from the second one (skip the script name)
-    for arg in sys.argv[1:]:
-        # Add the integer value of each argument to the total sum
-        total_sum += int(arg)
-    # Print the total sum followed by a new line
-    print(total_sum)
-
 if __name__ == "__main__":
-    infinite_add()
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
     
