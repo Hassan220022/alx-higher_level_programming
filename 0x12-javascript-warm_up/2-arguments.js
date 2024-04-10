3976e5d455746db15eddf9b70766df60de68ba69
Hassan220022 <hassansonson2002@gmail.com>
2024-04-10 03:19:24 +0200

    bor3i

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv.length;
-if (arg == 2) {
-  console.log("No argument");
+if (arg === 2) {
+  console.log('No argument');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
