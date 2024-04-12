87156384f0afcc16a510945b0c833a727b471f3d
Mikawi <90404558+Hassan220022@users.noreply.github.com>
2024-04-12 02:23:20 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
-const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
+const occurrences = {};
+
+for (const userId in dict) {
+  const occurrencesCount = dict[userId];
+  if (occurrences[occurrencesCount]) {
+    occurrences[occurrencesCount].push(userId);
+  } else {
+    occurrences[occurrencesCount] = [userId];
   }
-  newDict[valsUniq[j]] = list;
 }
-console.log(newDict);
+
+console.log(occurrences);
