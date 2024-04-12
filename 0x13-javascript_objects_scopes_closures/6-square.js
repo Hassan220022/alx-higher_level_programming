d62d3f7da40fec4a958584173a9f9e6300ceb304
Mikawi <90404558+Hassan220022@users.noreply.github.com>
2024-04-12 01:51:44 +0000

    square-withcharPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) { super(size, size); }
 
-  charPrint (character) {
-    if (character === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(character.repeat(this.width));
-      }
+const Square = require('./5-square');
+
+class SquareWithCharPrint extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
 
-module.exports = Square;
+module.exports = SquareWithCharPrint;
