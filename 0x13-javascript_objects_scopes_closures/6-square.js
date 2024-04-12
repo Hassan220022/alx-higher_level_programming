0e7f0ea7ed49e63e92e7f5129798a9823bf06ab8
Mikawi <90404558+Hassan220022@users.noreply.github.com>
2024-04-12 01:44:56 +0000

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) { super(size, size); }
+  charPrint () {
+    super.print();
+  }
+
+  charPrint (character) {
+    for (let i = 0; i < this.size; i++) {
+      console.log(character.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
