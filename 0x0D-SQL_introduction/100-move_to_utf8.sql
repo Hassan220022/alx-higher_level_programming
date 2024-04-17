422cd19216bcc093f90bff7f7256741a94b81ae7
Hassan220022 <hassansonson2002@gmail.com>
2024-04-17 20:04:43 +0200

    bor3i

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,12 @@
--- Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER
-SET
-    = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Convert the database character set and collation
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER
-SET
-    utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the table character set and collation
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER
-SET
-    utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Specifically convert the 'name' field in 'first_table' to use UTF8
+ALTER TABLE first_table
+    MODIFY name VARCHAR(256)
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
