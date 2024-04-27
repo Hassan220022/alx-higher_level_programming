e32b5d44a40ba24589610ad27abb551f358fc3c7
Hassan220022 <hassansonson2002@gmail.com>
2024-04-27 21:40:59 +0300

    done

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 import sys
+
+def print_board(board):
+    print([[i, board[i]] for i in range(len(board))])
 
 def is_safe(board, row, col):
     for i in range(col):
@@ -6,16 +10,37 @@ def is_safe(board, row, col):
             return False
     return True
 
-def solve_n_queens(board, col, n):
-    if col >= n:
-        print([[i, board[i]] for i in range(n)])
+def solve_n_queens(board, col):
+    if col == len(board):
+        print_board(board)
         return True
 
-    res = False
-    for i in range(n):
+    found_any = False
+    for i in range(len(board)):
         if is_safe(board, i, col):
             board[col] = i
-            res = solve_n_queens(board, col + 1, n) or res
-            board[col] = -1  # backtrack
+            found_any = solve_n_queens(board, col + 1) or found_any
+            board[col] = -1
+    return found_any
 
-    return res
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    if not solve_n_queens(board, 0):
+        print("No solution")
+
+if __name__ == "__main__":
+    main()
