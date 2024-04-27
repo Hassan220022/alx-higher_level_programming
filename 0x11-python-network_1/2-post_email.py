f144fef7fd22db6c4d34d549604a84e31cea1941
Hassan220022 <hassansonson2002@gmail.com>
2024-04-28 01:24:37 +0300

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,25 +5,17 @@ import urllib.request
 import urllib.parse
 
 
-def send_post_request(url, email):
-    """ Prepare data and send a POST request to the specified URL """
-    # Encode the email data to be sent with POST
-    data = urllib.parse.urlencode({'email': email}).encode()
-    
-    # Create a request object with the URL and data
-    req = urllib.request.Request(url, data=data)
-    
-    # Open the URL and send the POST request
-    with urllib.request.urlopen(req) as response:
-        # Read and decode the response
-        response_body = response.read().decode('utf-8')
-        print(response_body)
-
+import urllib.request
+import urllib.parse
+import sys
 
 if __name__ == "__main__":
-    # Extract URL and email from command line arguments
+
     url = sys.argv[1]
-    email = sys.argv[2]
-    
-    # Call the function to send POST request
-    send_post_request(url, email)
+    new_value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(new_value).encode()
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        val = response.read().decode('utf8')
+    print(val)
