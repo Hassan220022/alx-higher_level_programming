e92c9944467e987a2a7397fa69bf0b92e35611f5
Hassan220022 <hassansonson2002@gmail.com>
2024-04-27 21:43:01 +0300

    done

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,11 +5,13 @@ import sys
 def print_board(board):
     print([[i, board[i]] for i in range(len(board))])
 
+
 def is_safe(board, row, col):
     for i in range(col):
         if board[i] == row or abs(board[i] - row) == abs(i - col):
             return False
     return True
+
 
 def solve_n_queens(board, col):
     if col == len(board):
@@ -23,6 +25,7 @@ def solve_n_queens(board, col):
             found_any = solve_n_queens(board, col + 1) or found_any
             board[col] = -1
     return found_any
+
 
 def main():
     if len(sys.argv) != 2:
@@ -38,10 +41,3 @@ def main():
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    board = [-1] * n
-    if not solve_n_queens(board, 0):
-        print("No solution")
-
-if __name__ == "__main__":
-    main()
