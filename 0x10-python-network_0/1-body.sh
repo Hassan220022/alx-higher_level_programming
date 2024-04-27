b5f4adb56caa7232c3178b9cf9352f6ecde267d7
Hassan220022 <hassansonson2002@gmail.com>
2024-04-27 20:23:52 +0300

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# this script sends a GET request to a URL and displays the body of the response
-
-URL=$1
-STATUS_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" $URL)
-
-if [ $STATUS_CODE -eq 200 ]; then
-    curl -s $URL
-else
-    echo "Invalid status code: $STATUS_CODE"
+# This script sends a GET request to a URL and displays the body of the response only if the status code is 200
+response=$(curl -s -o response_body -w "%{http_code}" "$1")
+if [ "$response" -eq 200 ]; then
+    cat response_body
 fi
+rm response_body
