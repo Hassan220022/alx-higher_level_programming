05fe3a87889599810fcc062e7f735a1a0b873be9
Hassan220022 <hassansonson2002@gmail.com>
2024-04-27 21:47:03 +0300

    done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to post data (url-encoded) to a server
-curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+# Sends a POST request with email and subject variables and displays the response body.
+curl -s "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
