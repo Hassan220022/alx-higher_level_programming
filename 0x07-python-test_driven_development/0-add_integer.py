ca67187f8d134d54817431223d817bf69eefd48d
Hassan220022 <hassansonson2002@gmail.com>
2024-04-28 17:59:52 +0300

    bor3i

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+"""Defines a function add_integer(a, b=98) that adds two integers.
+
+Attributes:
+    add_integer: function that adds two integers.
+"""
 
 
 def add_integer(a, b=98):
+    """Adds two integer and/or float values.
+
+    Args:
+        a (int): First value
+        b (int, optional): Second value. Defaults to 98.
+
+    Raises:
+        TypeError: If a and b are not integers or floats.
+
+    Returns:
+        int: Sum of a and b.
     """
-    adds an integer
-    unit tests located in tests/0-add_integer.txt
-    checks for type errors
-    """
-    if not isinstance(a, int):
-        if isinstance(a, float):
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        if isinstance(b, float):
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
     return a + b
