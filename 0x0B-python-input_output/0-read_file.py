9a33b031e9e332876e357cff96a0256b78d7bac4
Hassan220022 <hassansonson2002@gmail.com>
2024-07-11 16:47:38 +0300

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import codecs
 def read_file(filename=""):
-    with codecs.open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return f.read()    
