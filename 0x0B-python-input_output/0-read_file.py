97c9510a9c86bd2395c38e88bb1c57f7539930db
Hassan220022 <hassansonson2002@gmail.com>
2024-07-11 17:21:03 +0300

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 """
-Module for reading and printing the contents of a file.
+Read file function
 """
 
 
 def read_file(filename=""):
+    """read a file
+    args:
+        filename: file to manipulate
+    return:
+        na
     """
-    Reads a file and prints its contents to the standard output.
 
-    Args:
-        filename (str): The name of the file to read.
-        Defaults to an empty string.
-
-    Raises:
-        FileNotFoundError: If the file does not exist.
-        IOError: If an I/O error occurs.
-    """
-    try:
-        with open(filename, "r", encoding="UTF-8") as f:
-            print(f.read(), end="\n")
-    except FileNotFoundError:
-        print(f"Error: The file '{filename}' does not exist.")
-    except IOError as e:
-        print(f"Error: {e}")
+    with open(filename, encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
